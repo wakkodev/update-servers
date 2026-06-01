12b697356c331c5953f0e7f6ac69c57ea0f35b81
WakkoDEV <87351045+wakkodev@users.noreply.github.com>
2026-05-31 22:29:24 -0500

    Update udp_manager.sh

--- udp_manager.sh
+++ udp_manager.sh
@@ -108,27 +108,27 @@ restart_server() {
 }
 
 uninstall_server() {
-    echo -e "\n\e[1;34mUninstalling AGN-UDP server...\e[0m"
+    echo -e "\n\e[1;34mUninstalling UDP Hysteria server...\e[0m"
     systemctl stop hysteria-server
     systemctl disable hysteria-server
     rm -f "$SYSTEMD_SERVICE"
     systemctl daemon-reload
     rm -rf "$CONFIG_DIR"
     rm -f /usr/local/bin/hysteria
-    echo -e "\e[1;32mAGN-UDP server uninstalled successfully.\e[0m"
+    echo -e "\e[1;32mUDP Hysteria server uninstalled successfully.\e[0m"
 }
 
 show_banner() {
     echo -e "\e[1;36m---------------------------------------------"
-    echo " AGNUDP Manager"
-    echo " (c) 2023 Khaled AGN"
-    echo " Telegram: @khaledagn"
+    echo " UDP Hysteria Manager"
+    echo " (c) 2026 WakkoDev"
+    echo " Telegram: @judabuac"
     echo "---------------------------------------------\e[0m"
 }
 
 show_menu() {
     echo -e "\e[1;36m----------------------------"
-    echo " AGNUDP Manager"
+    echo " UDP Hysteria Manager"
     echo -e "----------------------------\e[0m"
     echo -e "\e[1;32m1. Add new user"
     echo "2. Edit user password"
